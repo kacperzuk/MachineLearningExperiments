4d756687d5ae0c36a7c4a5266f065a55cd50b091
Bartosz Irzyk <bartosz.irzyk@wp.pl>
2015-11-28 18:10:57 +0100

    ports

--- Test Data/featureExtraction.py
+++ Test Data/featureExtraction.py
@@ -2,27 +2,20 @@ from data import samples
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 vectorizer = CountVectorizer(min_df=1)
-# outputIp = np.zeros(shape = [len(samples),4])
 outputIp = []
-
 hostnames = []
-
-
 
 for x in samples:
 	if len(x["hostnames"]) == 0:
 		hostnames.append("")
 	else:
-		[hostnames.append(y) for y in x["hostnames"]]
+		hostnames.append(x["hostnames"][0])
 
 X = vectorizer.fit_transform(hostnames)
 X = X.toarray()
-print(X[1][2])
 for i,val in enumerate(samples):
-	foo = ([int(y) for y in val["ip"].split(".")])
-	m = []
-	
-	foo = foo + [n for n in X[i]] + [n for n in val["ports"]]
+	foo = ([int(y) for y in val["ip"].split(".")])	
+	foo = foo + [n for n in X[i]] + [int(22 in val["ports"])] + [int(25 in val["ports"])] + [int(80 in val["ports"])] + [int(443 in val["ports"])] + [int(8080 in val["ports"])]
 	outputIp.append(foo)
  
 print(outputIp)
