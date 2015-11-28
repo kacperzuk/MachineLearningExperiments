dbd90fbf47841b208d145e069f29b59ed11dbf3f
Bartosz Irzyk <bartosz.irzyk@wp.pl>
2015-11-28 17:07:50 +0100

    transform hostnames

--- Test Data/featureExtraction.py
+++ Test Data/featureExtraction.py
@@ -5,15 +5,24 @@ vectorizer = CountVectorizer(min_df=1)
 # outputIp = np.zeros(shape = [len(samples),4])
 outputIp = []
 
-for val in samples:
-	foo = []
-	foo.append([int(y) for y in val["ip"].split(".")])
-	hosts = val["hostnames"]
-	if len(hosts) == 0:
-		foo.append([])
+hostnames = []
+
+
+
+for x in samples:
+	if len(x["hostnames"]) == 0:
+		hostnames.append("")
 	else:
-		foo.append(vectorizer.fit_transform(hosts))
+		[hostnames.append(y) for y in x["hostnames"]]
+
+X = vectorizer.fit_transform(hostnames)
+X = X.toarray()
+print(X[1][2])
+for i,val in enumerate(samples):
+	foo = ([int(y) for y in val["ip"].split(".")])
+	m = []
+	
+	foo = foo + [n for n in X[i]] + [n for n in val["ports"]]
 	outputIp.append(foo)
  
 print(outputIp)
-
