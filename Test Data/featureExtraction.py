9c05fc7bd12768950e6cb54982800e3db5026ff8
Bartosz Irzyk <bartosz.irzyk@wp.pl>
2015-11-29 19:35:25 +0100

    whois extraction

--- Test Data/featureExtraction.py
+++ Test Data/featureExtraction.py
@@ -1,10 +1,29 @@
 from data import samples
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction import DictVectorizer
 import numpy as np
+vec = DictVectorizer()
 vectorizer = CountVectorizer(min_df=1)
 outputIp = []
 hostnames = []
 ports = [22,25,80,443,8080]
+
+
+newDict = []
+for val in samples:
+	d = {}
+	for j,y in enumerate(val["whois"]):
+		for i,z in enumerate(y):
+			for key in y[z]:
+				if isinstance(y[z][key],list):
+					d[z + str(i) + key] = "".join(y[z][key])
+				else:
+					d[z + str(i) + key] = y[z][key]
+	newDict.append(d)		
+
+Y = vec.fit_transform(newDict)
+Y = Y.toarray()
+
 for x in samples:
 	if len(x["hostnames"]) == 0:
 		hostnames.append("")
@@ -15,7 +34,8 @@ X = vectorizer.fit_transform(hostnames)
 X = X.toarray()
 for i,val in enumerate(samples):
 	foo = ([int(y) for y in val["ip"].split(".")])	
-	foo = foo + [n for n in X[i]] + [int(x  in val["ports"]) for x in ports] 
+	foo = foo + [n for n in X[i]] + [m for m in Y[i]] + [int(x  in val["ports"]) for x in ports] 
 	outputIp.append(foo)
  
-print(outputIp)
+# print(outputIp)
+
