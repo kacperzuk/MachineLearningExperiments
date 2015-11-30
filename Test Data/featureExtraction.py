d6af420cbda714083f740f824ae8fa9464e60e34
Kacper Żuk <kacper.b.zuk@gmail.com>
2015-11-30 19:45:42 +0100

    Add coding: utf8

--- Test Data/featureExtraction.py
+++ Test Data/featureExtraction.py
@@ -1,3 +1,5 @@
+# -*- coding: utf8 -*-
+
 from data import samples
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction import DictVectorizer
@@ -10,11 +12,11 @@ ports = [22,25,80,443,8080]#otwarte porty jakie bierzemy pod uwage
 
 
 newDict = [] #lista wszystkich whois
-#te petle tworza niezagniezdzone slowniki z whoisa. Z racji ze w whois moze byc 2 odpowiedzi 
+#te petle tworza niezagniezdzone slowniki z whoisa. Z racji ze w whois moze byc 2 odpowiedzi
 #nowe unikalne klucze towrzne sa :element whoisa - z ( data lub server), inedx elementu whoisa j
 #stary klucz - key
 #przyklad : server0host : "whois.arin.net"
-# jesli jakis element jest tablica np klucz comment, nowa wartość klucza np data0comment to 
+# jesli jakis element jest tablica np klucz comment, nowa wartość klucza np data0comment to
 # polaczenie wszyzstkich elementow tej tablicy
 for val in samples:
 	d = {}
@@ -25,7 +27,7 @@ for val in samples:
 					d[z + str(j) + key] = "".join(y[z][key])
 				else:
 					d[z + str(j) + key] = y[z][key]
-	newDict.append(d)		
+	newDict.append(d)
 #transformacja slownika na macierz liczb
 Y = vec.fit_transform(newDict)
 Y = Y.toarray()
@@ -42,9 +44,9 @@ X = vectorizer.fit_transform(hostnames)
 X = X.toarray()
 #stowrzenie macierzy nxm gdzie n -liczba probek m - ilosc feature'ow
 for i,val in enumerate(samples):
-	foo = ([int(y) for y in val["ip"].split(".")])	
-	foo = foo + [n for n in X[i]] + [m for m in Y[i]] + [int(x  in val["ports"]) for x in ports] 
-	outputIp.append(foo)
- 
-# print(outputIp)
+  foo = ([int(y) for y in val["ip"].split(".")])
+  foo = foo + [n for n in X[i]] + [m for m in Y[i]] + [int(x  in val["ports"]) for x in ports]
+  outputIp.append(foo)
+
+#print(outputIp)
 
