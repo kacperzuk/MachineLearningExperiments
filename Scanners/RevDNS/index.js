df1646806fa6d2e9fccf01d179e422693ef6478f
Jakub Krol <jakub.krol@poczta.fm>
2015-12-06 08:16:57 -0800

    remove request("url") remove indentation errors

--- Scanners/RevDNS/index.js
+++ Scanners/RevDNS/index.js
@@ -1,33 +1,30 @@
 // Author: Jakub Król <jakub.krol@poczta.fm>
 
-var url = require ("url");
 var dns = require("dns");
 var express = require("express");
 var app = express();
 
-app.get('/:ip', function(request, response){
+app.get("/:ip", function(request, response){
 
-	var my_ip = request.params.ip;
+  var my_ip = request.params.ip;
 
-	var success = {"status":"ok", "hostnames":"hostnames"};
-	var failure = {"status":"tryagain", "reason":"reason"};
+  var success = {"status":"ok", "hostnames":"hostnames"};
+  var failure = {"status":"tryagain", "reason":"reason"};
 
-		dns.reverse(my_ip, function(err, hostnames){
+  dns.reverse(my_ip, function(err, hostnames){
 
-			if (err){
+    if (err){
 
-			failure.reason = err.message;
-			response.send(JSON.stringify(failure));
+      failure.reason = err.message;
+      response.send(JSON.stringify(failure));
 
-			return;
-			}
+      return;
+    }  else{
 
-			else{
-
-			success.hostnames = hostnames;
-			response.send(JSON.stringify(success));
-			}
-		});
+      success.hostnames = hostnames;
+      response.send(JSON.stringify(success));
+    }
+  });
 
 });
 
