2731ac8927455ea76309eea89af570fd25819199
Jakub Krol <jakub.krol@poczta.fm>
2015-12-08 13:46:52 -0800

    Add new scanner

--- Scanners/Whois/whois.js
+++ Scanners/Whois/whois.js
@@ -1,19 +1,24 @@
 // Author: Jakub Król <jakub.krol@poczta.fm>
-var whois = require("./whois-json.js");
+var whois = require("whois-json");
 var express = require("express");
 var app = express();
 
 app.get("/:url", function(request, response){
 
+  var success = {"status":"ok", "whois":"whois"};
+  var failure = {"status":"tryagain", "reason":"reason"};
+
   var url = request.params.url;
   whois(url, function(err, result){
 
     if(err){
-      response.send(err.message);
+      failure.reason = err.message;
+      response.send(failure);
     }else{
-      response.send(JSON.stringify(result, null, 2));
+      success.whois = result;
+      response.send(result);
     }
   });
 });
-app.listen(3000);
+app.listen(2000);
 console.log("server running on 3000");
