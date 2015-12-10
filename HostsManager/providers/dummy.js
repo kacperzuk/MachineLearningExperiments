5ee2e2bfb131fe99f2f84537ff9f3a9f962571ad
Kacper Żuk <kacper.b.zuk@gmail.com>
2015-12-10 23:40:51 +0100

    Providers must be async

--- HostsManager/providers/dummy.js
+++ HostsManager/providers/dummy.js
@@ -1,9 +1,11 @@
-function destroyInstance(h) {
+function destroyInstance(h, cb) {
   console.log("Destroying instance", h);
+  cb(null);
 }
 
-function createInstance(h) {
+function createInstance(h, cb) {
   console.log("Creating instance", h);
+  cb(null);
 }
 
 module.exports = { destroyInstance, createInstance };
