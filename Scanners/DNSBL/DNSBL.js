59c689122fe09e6eda42459b4fd587f179d4871f
Kacper Żuk <kacper.b.zuk@gmail.com>
2016-01-09 21:33:57 +0100

    Drop slow DNSBLs

--- Scanners/DNSBL/DNSBL.js
+++ Scanners/DNSBL/DNSBL.js
@@ -2,26 +2,24 @@ var express = require("express");
 var app = express();
 var dns = require("dns");
 var async = require("async");
-var blacklists = require("./blacklists.json");
+var blacklists = require("./blacklists");
 
 app.get("/:ip", function(request, response){
-
-  var ip = request.params.ip.split(".").reverse().join("."); 
+  var ip = request.params.ip.split(".").reverse().join(".");
   var result = {"status" : "ok", "blacklists":{}};
   async.each(blacklists, function (blacklist, callback){
+    var s = Date.now();
     dns.lookup(ip+blacklist, function(err){
-
-      if(err){
-        result.blacklists[blacklist] = "false";
-      } else {
-        result.blacklists[blacklist] = "true";
+      var t = (Date.now() - s)/1000;
+      if(t > 1) { // slooow DNSBL
+        console.log(blacklist, "is slow, consider dropping it");
       }
+      result.blacklists[blacklist] = !err;
       callback();
     });
   },
-    function(){
-      response.send(result);
-    });
-
+  function(){
+    response.send(result);
+  });
 });
 app.listen(4002);
