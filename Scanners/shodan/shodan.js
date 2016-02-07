64c758a676a01c0024aca906f8aa94ae5f4b8564
Jakub Krol <jakub.krol@poczta.fm>
2016-02-08 00:26:36 +0100

    improve error handling

--- Scanners/shodan/shodan.js
+++ Scanners/shodan/shodan.js
@@ -8,15 +8,12 @@ app.get("/:ip", function(request, response){
   var ip = request.params.ip;
   var options = {hostname: "api.shodan.io", path: "/shodan/host/" + ip + "?key=aH7F5pcxsC3U9i7hmPUYA6vwdehxxNeP"};
   var result = {status: "tryagain", shodan: "shodan"};
+ //for (var i = 0; i < 10000; i++) {
+
   https.get(options, function(res){
     var buffer = "";
     res.on("data", function(data){
     	buffer += data.toString();
-    });
-    res.on("error", function(error){
-    		result.status = "ok";
-    		result.shodan = null;
-    		response.send(result);
     });
     res.on("end", function(){
       buffer = JSON.parse(buffer.toString());
@@ -24,6 +21,9 @@ app.get("/:ip", function(request, response){
       result.shodan = buffer;
       response.send(result);
     })
-
-  });
+  }).on("error", function(error){
+  	result.shodan = error.message;
+  	response.send(result);
+  })
+//}
 }).listen(4004);
