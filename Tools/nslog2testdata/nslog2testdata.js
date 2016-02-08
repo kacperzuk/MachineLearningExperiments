62cd6ca9c5d686dbf147293fce92f09fb7994daf
Kacper Żuk <kacper.b.zuk@gmail.com>
2016-02-08 20:44:13 +0100

    Use facade in nslog2testdata

--- Tools/nslog2testdata/nslog2testdata.js
+++ Tools/nslog2testdata/nslog2testdata.js
@@ -8,11 +8,7 @@ const byline = require("byline"),
 http.globalAgent.maxSockets = 100;
 
 const logstream = byline(fs.createReadStream('log.txt'));
-const scanners = {
-  "hostnames": "http://127.0.0.1:4000",
-  "whois": "http://127.0.0.1:4001",
-  "dnsbl": "http://127.0.0.1:4002"
-};
+const facade = "http://127.0.0.1:3000";
 
 
 const addresses = [];
@@ -32,30 +28,26 @@ logstream.on('data', (line) => {
 
 var processed_addresses = [];
 
-
 logstream.on('end', () => {
   function processBatch() {
     const promises = [];
-    const batch = addresses.splice(0, 20);
+    const batch = addresses.splice(0, 50);
     const start = Date.now();
     batch.forEach((addr) => {
-      for(let key in scanners) {
-        let scanner = scanners[key];
-        promises.push(new Promise((resolve) => {
-          http.request(scanner+"/"+addr.ip, (res) => {
-            let body = new Buffer(0);
-            res.on('data', (d) => { body = Buffer.concat([body, d]); });
-            res.on('end', () => {
-              const res = JSON.parse(body);
-              addr[key] = null;
-              if(res.status === "ok") {
-                addr[key] = res[key];
-              }
-              resolve();
-            });
-          }).end();
-        }));
-      };
+      promises.push(new Promise((resolve) => {
+        http.request(facade+"/"+addr.ip, (res) => {
+          let body = new Buffer(0);
+          res.on('data', (d) => { body = Buffer.concat([body, d]); });
+          res.on('end', () => {
+            const res = JSON.parse(body);
+            addr.data = null;
+            if(res.status === "ok") {
+              addr.data = res;
+            }
+            resolve();
+          });
+        }).end();
+      }));
     });
     Promise.all(promises).then(() => {
       console.log("Left:", addresses.length, "\t\tTime to process batch of",batch.length,":", (Date.now() - start)/1000);
