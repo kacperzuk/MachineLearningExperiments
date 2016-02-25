af752df1107faad76aa58cd63187acd9d6482282
Jakub Krol <jakub.krol@poczta.fm>
2016-02-25 20:12:10 +0100

    add some logic

--- NastyHosts2/decision_logic.py
+++ NastyHosts2/decision_logic.py
@@ -1,3 +1,32 @@
 def process(data):
-        result = {"suggestion" : "allow/deny", "factor" : 123}
-        return result
+
+    factor = 1
+    text = ""
+    dangers = ["vps", "vpn", "hosting", "server", "bot", "proxy", "amazon"]
+    ports = [22, 80, 443, 587, 465, 8080]
+    for whois_data in data["whois"]["whois"]:
+        for value in whois_data["data"].values():
+            text += value
+
+    for revdns_data in data["revdns"]["hostnames"]:
+            text += revdns_data
+
+    for danger in dangers:
+        if(danger in text):
+            factor *= 0.1
+
+    for blacklist in data ["dnsbl"]["blacklists"]:
+        if(data ["dnsbl"]["blacklists"][blacklist]):
+            factor *= 0.8
+
+    if ("ports" in data["shodan"]["shodan"]):
+        for port in data["shodan"]["shodan"]["ports"]:
+            if (port in ports):
+                factor *= 0.7
+
+    if (factor < 0.5):
+        result = {"suggestion" : "deny", "factor" : factor}
+    else:
+        result = {"suggestion" : "allow", "factor" : factor}
+    
+    return result
