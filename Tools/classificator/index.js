e1c17ca4f2ef2484c17883f63c227f5deeeffaea
Kacper Żuk <kacper.b.zuk@gmail.com>
2016-03-05 16:49:44 +0100

    Classificator v1

---
+++ Tools/classificator/index.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var dns = require("dns");
+var whois = require("whois-json");
+var https = require("https");
+var pg = require("pg");
+var argv = require("minimist")(process.argv.slice(2));
+
+var app = express();
+
+var pgclient;
+
+app.engine('handlebars', exphbs({defaultLayout: false}));
+app.set('view engine', 'handlebars');
+
+app.get('/classify/:class/:ip', (req, res) => {
+
+});
+
+app.get('/shodan/:ip', (req, res) => {
+  https.get(`https://api.shodan.io/shodan/host/${req.params.ip}?key=aH7F5pcxsC3U9i7hmPUYA6vwdehxxNeP`, (r) => {
+    var buf = "";
+    r.on("data", (d) => buf += d.toString());
+    r.on("end", () => {
+      res.send(JSON.stringify(JSON.parse(buf), null, 2));
+    });
+  });
+});
+
+app.get('/whois/:ip', (req, res) => {
+  whois(req.params.ip, {verbose: true}, (err, result) => {
+    if(err)
+      res.send(err);
+    else
+      res.send(JSON.stringify(result, null, 2));
+  });
+});
+
+app.get('/revdns/:ip', (req, res) => {
+  dns.reverse(req.params.ip, (err, hostnames) => {
+    if(!hostnames || hostnames.length < 1) {
+      res.send('brak revdns');
+    } else {
+      res.send(hostnames[0]);
+    }
+  });
+});
+
+app.get('/', (req, res) => {
+  pgclient.query("SELECT ip FROM niesklasyfikowane OFFSET random() * (select count(*) from niesklasyfikowane) limit 1", (err, result) => {
+    var ip = result.rows[0].ip;
+    res.render('index', {ip});
+  });
+});
+
+pg.connect(argv.pg_host, function(err, client, done) {
+  if(err) throw err;
+  pgclient = client;
+  app.listen(8000);
+  console.log("Ready on 8000");
+});
