b33a4c0f96760752ba9538825be7a9ddf61a05ea
Kacper Żuk <kacper.b.zuk@gmail.com>
2016-03-10 00:49:23 +0100

    New test data

--- Tools/db-scans/dbscans2pydata.js
+++ Tools/db-scans/dbscans2pydata.js
@@ -6,25 +6,66 @@ const fs = require("fs");
 
 pg.connect(argv.pg_host, function(err, client, done) {
   if(err) throw err;
-  console.log("Fetching data...");
-  client.query("SELECT scans.ip, bot = -1 as bot, result FROM scans INNER JOIN adresy ON adresy.ip = scans.ip AND adresy.bot is not null AND adresy.bot <> 0", (query_err, res) => {
-    if(query_err) throw query_err;
-    console.log("Fetched", res.rows.length, "rows.");
-    console.log("Parsing data...");
-    const addresses = res.rows.map((v) => {
-      return {
-        ip: v.ip,
-        bot: v.bot,
-        data: JSON.parse(v.result)
-      };
+  console.log("Counting data...");
+  client.query("SELECT (SELECT COUNT(*) FROM adresy WHERE bot = 1) as nonbots, (SELECT COUNT(*) FROM adresy WHERE bot = -1) as bots", (err, res) => {
+    if(err) throw err;
+    res = res.rows[0];
+    let bots_count = parseInt(res.bots);
+    let nonbots_count = parseInt(res.nonbots);
+    let total_count = bots_count + nonbots_count;
+
+    if(argv.ratio) {
+      let current_ratio = bots_count/total_count;
+      argv.ratio /= 100;
+      if(current_ratio > argv.ratio) {
+        bots_count = parseInt(nonbots_count / (1-argv.ratio) * argv.ratio);
+      } else {
+        nonbots_count = parseInt(bots_count / argv.ratio * (1 - argv.ratio));
+      }
+    }
+
+    total_count = bots_count + nonbots_count;
+    let ratio = bots_count / total_count;
+
+    console.log("DB data:", res);
+    console.log("Used data: ", { nonbots_count, bots_count });
+
+    console.log("Fetching data...");
+    client.query(`
+      WITH data AS (
+        SELECT scans.ip, bot = -1 as bot, result FROM scans INNER JOIN adresy ON adresy.ip = scans.ip AND adresy.bot is not null AND adresy.bot <> 0
+      )
+      (SELECT * FROM data WHERE bot = 't' LIMIT ${bots_count})
+      UNION
+      (SELECT * FROM data WHERE bot = 'f' LIMIT ${nonbots_count})
+    `, (err, res) => {
+      if(err) throw err;
+      console.log("Fetched", res.rows.length, "rows.");
+      console.log("Parsing data...");
+      const addresses = res.rows.map((v) => {
+        return {
+          ip: v.ip,
+          bot: v.bot,
+          data: JSON.parse(v.result)
+        };
+      });
+      console.log("Parsed.");
+      console.log("Formatting data...");
+      let data = "# -*- coding:utf8 -*-";
+      data += "\n#Number of bots: "+bots_count;
+      data += "\n#Number of nonbots: "+nonbots_count;
+      data += "\n#Total: "+total_count;
+      data += "\n#Bots to total ratio: "+ratio;
+      data += "\n\nsamples = ";
+      data += JSON.stringify(addresses, null, 4)
+                  .replace(/false/g, "False")
+                  .replace(/null/g, "None")
+                  .replace(/true/g, "True");
+      console.log("Writing data...");
+      fs.writeFileSync("data.py", data);
+      console.log("Done");
+      done();
+      pg.end();
     });
-    console.log("Parsed.");
-    console.log("Formatting data...");
-    const data = "# -*- coding:utf8 -*-\n\nsamples = "+JSON.stringify(addresses, null, 4).replace(/false/g, "False").replace(/null/g, "None").replace(/true/g, "True")
-    console.log("Writing data...");
-    fs.writeFileSync("data.py", data);
-    console.log("Done");
-    done();
-    pg.end();
   });
 });
