0cfafcb34b9ec6a290138f074e5a16293c851785
Kacper Żuk <kacper.b.zuk@gmail.com>
2016-03-10 13:37:18 +0100

    Update scan.js for new database, handle errors in nh1, handle errors in getipintel. nh2/nhml not done

--- Tools/comparator/scan.js
+++ Tools/comparator/scan.js
@@ -3,111 +3,124 @@
 const request = require("request");
 const pg = require("pg");
 const argv = require("minimist")(process.argv.slice(2));
-const stdin = require("byline")(process.stdin);
 
-const urls = {
-  nh2: argv.nh2_url || "http://127.0.0.1:5000/",
-  nhml: argv.nhml_url || "http://127.0.0.1:5001/",
-  nh1: "http://v1.nastyhosts.com/",
-  getipintel: "http://check.getipintel.net/check.php?contact=xyz@gmail.com&format=json&flags=b&ip="
-};
+let client;
 
-if(!(argv.nh2 || argv.nh1 || argv.nhml || argv.getipintel)) {
-  console.log("Usage:");
-  console.log("   ", process.argv[1], "[--pg_host=host] [services]");
-  console.log("Services (at least one required):");
-  console.log("    --nh2");
-  console.log("    --nh1");
-  console.log("    --nhml");
-  console.log("    --getipintel");
+const services = {
+  nh1: (ip) => {
+    return new Promise((resolve) => {
+      request({
+        uri: "http://v1.nastyhosts.com/"+ip,
+        time: true
+      }, function(err, resp, body) {
+        body = JSON.parse(body);
+        if(body.status != 200) {
+          console.log(body);
+          process.exit(2);
+        }
+        let timeToProcess = resp.elapsedTime;
+        let suggestion = body.suggestion;
+        let factor = suggestion == "allow" ? 1 : 0;
+
+        upsert([ip, 'nh1', timeToProcess, suggestion, factor], resolve);
+      });
+    });
+  },
+  nh2: (ip) => {
+    const url = argv.nh2_url || "http://127.0.0.1:5000/";
+    return new Promise((resolve) => {
+      request({
+        uri: url+ip,
+        time: true
+      }, function(err, resp, body) {
+        body = JSON.parse(body);
+        let timeToProcess = resp.elapsedTime;
+        let suggestion = body.suggestion;
+        let factor = body.factor;
+        upsert([ip, 'nh2', timeToProcess, suggestion, factor], resolve);
+      });
+    });
+  },
+  getipintel: (ip) => {
+    let url = "http://check.getipintel.net/check.php?contact=xyz@gmail.com&format=json&flags=b&ip=";
+    return new Promise((resolve) => {
+      request({
+        uri: url+ip,
+        time: true
+      }, function(err, resp, body) {
+        if(!body) {
+          console.log("Getipintel empty body (rate limit hit)");
+          process.exit(3);
+        }
+        body = JSON.parse(body);
+        if(body.status != 'success') {
+          console.log(body);
+          process.exit(2);
+        }
+        let timeToProcess = resp.elapsedTime;
+        let suggestion = body.result < 0.5 ? 'allow' : 'deny';
+        let factor = 1 - parseFloat(body.result);
+        upsert([ip, 'getipintel', timeToProcess, suggestion, factor], resolve);
+      });
+    });
+  },
+  nhml: (ip) => {
+    let url = argv.nhml_url || "http://127.0.0.1:5001/";
+    return false;
+  }
+}
+
+function upsert(data, callback) {
+  console.log(data);
+  client.query("SELECT 1 FROM results WHERE ip=$1 AND service=$2", data.slice(0, 2), (err, result) => {
+    if(result.rows.length > 0)
+      client.query("UPDATE results SET time=$3, suggestion=$4, factor=$5 WHERE ip=$1 AND service=$2", data, callback);
+    else
+      client.query("INSERT INTO results (ip, service, time, suggestion, factor) VALUES ($1, $2, $3, $4, $5)", data, callback);
+  });
+}
+
+function getStats() {
+  client.query(`SELECT
+      (SELECT COUNT(*) FROM results WHERE service = $1) as done,
+      (SELECT COUNT(*) FROM adresy WHERE bot IN (1,-1)) as total
+  `, [argv.service], (err, res) => {
+    if(err) throw err;
+    console.log(res.rows);
+  });
+}
+
+function processBatch(done) {
+  console.log("Using service", argv.service);
+  getStats();
+  client.query("SELECT ip FROM adresy WHERE bot IN (1,-1) AND NOT EXISTS (SELECT 1 FROM results WHERE results.ip = adresy.ip AND service = $1) LIMIT 15", [argv.service], (err, res) => {
+    if(err) throw err;
+    if(res.rows.length == 0) {
+      done();
+    } else {
+      const promises = [];
+      res.rows.forEach((row) => {
+        promises.push(services[argv.service](row.ip));
+      });
+      Promise.all(promises).then(() => {
+        processBatch(done);
+      });
+    }
+  });
+}
+
+if(!argv.service) {
+  console.log("--service=<service> required!!");
   process.exit(1);
 }
 
-pg.connect(argv.pg_host, function(err, client, done) {
+pg.connect(argv.pg_host, function(err, pgclient, done) {
+  client = pgclient;
   if(err) throw err;
 
-  client.query(`
-    CREATE TABLE IF NOT EXISTS scans (
-      ip text,
-      service text,
-      time integer,
-      suggestion text,
-      factor real,
-      primary key (ip, service)
-    );
-  `);
-
-  function upsert(data, callback) {
-    console.log(data);
-    client.query("SELECT 1 FROM scans WHERE ip=$1 AND service=$2", data.slice(0, 2), (err, result) => {
-      if(result.rows.length > 0)
-        client.query("UPDATE scans SET time=$3, suggestion=$4, factor=$5 WHERE ip=$1 AND service=$2", data, callback);
-      else
-        client.query("INSERT INTO scans (ip, service, time, suggestion, factor) VALUES ($1, $2, $3, $4, $5)", data, callback);
-    });
-  }
-
-  function scanIP(ip) {
-    let promises = [];
-
-    if(argv.nh1) {
-      promises.push(new Promise((resolve) => {
-        request({
-            uri: urls.nh1+ip,
-            time: true
-        }, function(err, resp, body) {
-          body = JSON.parse(body);
-          let timeToProcess = resp.elapsedTime;
-          let suggestion = body.suggestion;
-          let factor = suggestion == "allow" ? 1 : 0;
-
-          upsert([ip, 'nh1', timeToProcess, suggestion, factor], resolve);
-        });
-      }));
-    }
-
-    if(argv.getipintel) {
-      promises.push(new Promise((resolve) => {
-        request({
-            uri: urls.getipintel+ip,
-            time: true
-        }, function(err, resp, body) {
-          if(!body) {
-            console.log("Getipintel empty body");
-            return;
-          }
-          body = JSON.parse(body);
-          let timeToProcess = resp.elapsedTime;
-          let suggestion = body.result < 0.5 ? 'allow' : 'deny';
-          let factor = 1 - parseFloat(body.result);
-          upsert([ip, 'getipintel', timeToProcess, suggestion, factor], resolve);
-        });
-      }));
-    }
-
-    if(argv.nh2) {
-      promises.push(new Promise((resolve) => {
-        request({
-            uri: urls.nh2+ip,
-            time: true
-        }, function(err, resp, body) {
-          body = JSON.parse(body);
-          let timeToProcess = resp.elapsedTime;
-          let suggestion = body.suggestion;
-          let factor = body.factor;
-          upsert([ip, 'nh2', timeToProcess, suggestion, factor], resolve);
-        });
-      }));
-    }
-
-    return Promise.all(promises);
-  }
-
-  let promises = [];
-  stdin.on('data', (line) => {
-    promises.push(scanIP(line.toString().trim()));
-  });
-  stdin.on('end', () => {
-    Promise.all(promises).then(() => { done(); pg.end() });
+  processBatch(() => {
+    console.log("Done");
+    done();
+    pg.end();
   });
 });
