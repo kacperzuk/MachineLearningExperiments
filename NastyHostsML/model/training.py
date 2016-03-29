bbc9e4e2ff11d11af75100a88edfc4b78f4a05a3
Kacper Żuk <kacper.b.zuk@gmail.com>
2016-03-29 19:43:20 +0200

    Trees Classifier

--- NastyHostsML/model/training.py
+++ NastyHostsML/model/training.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import cPickle as pickle
-from sklearn.svm import SVC
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.cross_validation import train_test_split
 
@@ -13,7 +13,7 @@ raw_y = np.load("../../Test Data/2016-03-24/ratio_25/bots.npy")
 
 X_train, X_test, y_train, y_test = train_test_split(raw_X, raw_y, test_size = 0.4, random_state = 5)
 
-clf = SVC(kernel='rbf', C=1000, gamma=0.005, coef0=0.0)
+clf = ExtraTreesClassifier(n_estimators=500, criterion='entropy', min_samples_split=2, min_samples_leaf=1)
 clf.fit(X_train, y_train)
 pred = clf.predict(X_test)
 acc = accuracy_score(pred, y_test)
